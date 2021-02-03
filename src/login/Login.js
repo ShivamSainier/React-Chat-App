338a47a836fa701d84158ffcb755a95b6a19ac33
shivam saini <59383963+shivam007-ss@users.noreply.github.com>
2021-02-03 10:09:36 +0530

    Add  Login page

---
+++ src/login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class LoginComponent extends React.Component{
+    render(){
+        return (
+            <>
+            <h2>Login Component</h2>
+            </>
+        )
+    }
+
+}
+export default LoginComponent
