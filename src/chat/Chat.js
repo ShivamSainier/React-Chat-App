600f5edf1467a15975e1507f4dcbf7c440e7be4b
shivam saini <59383963+shivam007-ss@users.noreply.github.com>
2021-02-04 15:01:41 +0530

    Create a new compoent called chat to hold the chat details

---
+++ src/chat/Chat.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Paper,FormControl,InputLabel,Input,Typography,Button ,CssBaseline } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import style from './style';
+import firebase from 'firebase'
+import {Link} from "react-router-dom"
+
+
+
+
+class Chat extends React.Component {
+    render() {
+        return (
+            <div>
+                chat list
+            </div>
+        )
+    }
+}
+
+export default withStyles(style)(Chat)
