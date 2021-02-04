1850937caf79907c129c4426941122ffffcf3ae5
shivam saini <59383963+shivam007-ss@users.noreply.github.com>
2021-02-04 21:54:07 +0530

    add chat page

--- src/chat/Chat.js
+++ src/chat/Chat.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Paper,FormControl,InputLabel,Input,Typography,Button ,CssBaseline } from '@material-ui/core';
+import {Paper,FormControl,InputLabel,Input,Typography,Button ,CssBaseline,List,ListItem,ListItemAvatar,Avatar, ListItemText, Divider } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import style from './style';
 import firebase from 'firebase'
@@ -10,11 +10,54 @@ import {Link} from "react-router-dom"
 
 class Chat extends React.Component {
     render() {
+        const {classes}=this.props 
         return (
-            <div>
-                chat list
-            </div>
+            <main className={classes.root}>
+                <Button variant="contained" fullWidth color="primary" className={classes.newChatBtn} onClick={this.newChat }></Button>
+                <List>{
+                    this.props.chat.map((_chat,_index)=>{
+                        return (
+                            <div key={_index}>
+                            <ListItem onClick={()=>this.selectChat} className={classes.listItem} 
+                            selected={this.props.selectedChatIndex===_index}
+                            alignItems='flex-start'>
+                            <ListItemAvatar>
+                            <Avatar alt="saini">
+                                {_chat.users.filter(_user=>_user!==this.props.userEmail)[0].split('')[0]}
+                            </Avatar>
+
+                            </ListItemAvatar>
+                             <ListItemText primary={_chat.users.filter(_user=>_user!==this.props.userEmail)[0]} 
+                             secondary={
+                                 <>
+                                 <Typography>
+                                     {
+                                         _chat.messages[_chat.messages.length]
+                                     }
+                                 </Typography>
+                                 </>
+                             }>
+
+                             </ListItemText>
+                            
+
+                            </ListItem>
+ 
+                            </div>
+                            
+                        )
+                    })
+                }
+                </List>
+            </main> 
         )
+    }
+    newChat=()=>{
+
+    }
+    selectChat=(index)=>{
+        console.log("selet chat",index)
+
     }
 }
 
