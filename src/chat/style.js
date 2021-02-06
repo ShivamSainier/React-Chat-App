3d6fdb11112ee59ce459aebb98fa10d9efddd20b
shivam saini <59383963+shivam007-ss@users.noreply.github.com>
2021-02-06 11:33:53 +0530

    Change style

--- src/chat/style.js
+++ src/chat/style.js
@@ -1,42 +1,23 @@
 const styles = theme => ({
-    main: {
-      width: 'auto',
-      display: 'block', // Fix IE 11 issue.
-      marginLeft: theme.spacing.unit * 3,
-      marginRight: theme.spacing.unit * 3,
-      [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
-        width: 400,
-        marginLeft: 'auto',
-        marginRight: 'auto',
-      },
-    },
-    paper: {
-      marginTop: theme.spacing.unit * 8,
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
-    },
-    form: {
-      width: '100%',
-      marginTop: theme.spacing.unit,
-    },
-    submit: {
-      marginTop: theme.spacing.unit * 3,
-    },
-    hasAccountHeader: {
-      width: '100%'
-    },
-    logInLink: {
-      width: '100%',
-      textDecoration: 'none',
-      color: '#303f9f',
-      fontWeight: 'bolder'
-    },
-    errorText: {
-      color: 'red',
-      textAlign: 'center'
-    }
-  });
-  
-  export default styles;
+  root: {
+    backgroundColor: theme.palette.background.paper,
+    height: 'calc(100% - 35px)',
+    position: 'absolute',
+    left: '0',
+    width: '300px',
+    boxShadow: '0px 0px 2px black'
+  },
+  listItem: {
+    cursor: 'pointer'
+  },
+  newChatBtn: {
+    borderRadius: '0px'
+  },
+  unreadMessage: {
+    color: 'red',
+    position: 'absolute',
+    top: '0',
+    right: '5px'
+  }
+});
+export default styles
