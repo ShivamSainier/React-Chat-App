08a3071d31741f40c971f6aa2acb4cff94101ba8
shivam saini <59383963+shivam007-ss@users.noreply.github.com>
2021-02-06 11:34:05 +0530

    Add selected chat fn

--- src/chat/Chat.js
+++ src/chat/Chat.js
@@ -1,46 +1,41 @@
 import React from 'react'
 import {Paper,FormControl,InputLabel,Input,Typography,Button ,CssBaseline,List,ListItem,ListItemAvatar,Avatar, ListItemText, Divider } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
-import style from './style';
 import firebase from 'firebase'
 import {Link} from "react-router-dom"
-
-
+import styles from "./style"
 
 
 class Chat extends React.Component {
     render() {
         const {classes}=this.props 
         return (
-            <main className={classes.root}>
+            <div className={classes.root}>
                 <Button variant="contained" fullWidth color="primary" className={classes.newChatBtn} onClick={this.newChat }></Button>
                 <List>{
                     this.props.chat.map((_chat,_index)=>{
                         return (
-                            <div key={_index}>
-                            <ListItem onClick={()=>this.selectChat} className={classes.listItem} 
-                            selected={this.props.selectedChatIndex===_index}
-                            alignItems='flex-start'>
-                            <ListItemAvatar>
-                            <Avatar alt="saini">
-                                {_chat.users.filter(_user=>_user!==this.props.userEmail)[0].split('')[0]}
-                            </Avatar>
-
-                            </ListItemAvatar>
-                             <ListItemText primary={_chat.users.filter(_user=>_user!==this.props.userEmail)[0]} 
-                             secondary={
-                                 <>
-                                 <Typography>
-                                     {
-                                         _chat.messages[_chat.messages.length]
-                                     }
-                                 </Typography>
-                                 </>
-                             }>
-
-                             </ListItemText>
-                            
-
+                        <div key={_index}>
+                        <ListItem onClick={()=>this.selectChat(_index)} className={classes.listItem} 
+                        selected={this.props.selectedChatIndex===_index}
+                        alignItems='flex-start'>
+                        <ListItemAvatar>
+                        <Avatar alt="saini">
+                            {_chat.users.filter(_user=>_user!==this.props.userEmail)[0].split('')[0]}
+                        </Avatar>
+                        </ListItemAvatar>
+                         <ListItemText primary={_chat.users.filter(_user=>_user!==this.props.userEmail)[0]} 
+                         secondary={
+                             <>
+                             <Typography>
+                                 {
+                                     _chat.messages[_chat.messages.length]
+                                 }
+                             </Typography>
+                             </>
+                         }>
+                         </ListItemText>
+                        
                             </ListItem>
  
                             </div>
@@ -49,16 +44,16 @@ class Chat extends React.Component {
                     })
                 }
                 </List>
-            </main> 
+            </div> 
         )
     }
     newChat=()=>{
 
     }
     selectChat=(index)=>{
-        console.log("selet chat",index)
+        this.props.selectChatFn(index)
 
     }
 }
 
-export default withStyles(style)(Chat)
+export default withStyles(styles)(Chat)
